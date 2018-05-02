751f6b9aaef1969a56034ed2e665da27e969f640
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-02 15:32:00 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,7 @@ class Artist
   @@artists = []
 
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.all.detect{|a| a.name == name}
   end
 
   def initialize
@@ -24,7 +24,7 @@ class Artist
   end
 
   def self.count
-    @@artists.count
+    self.all.count
   end
 
   def add_song(song)
