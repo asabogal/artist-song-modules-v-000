54d9dd38d9cb669bf2165f68f7fd83281b33a911
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-02 16:15:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,24 +17,9 @@ class Song
     @@songs
   end
 
-  # def self.find_by_name(name)
-  #   self.all.detect{|a| a.name == name}
-  # end
-
   def artist=(artist)
     @artist = artist
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
-
+  
 end
